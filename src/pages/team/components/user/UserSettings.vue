58a5bd59c41482239dd3b75747a40844a2d5ca3a
jooler <jerr@foxmail.com>
2024-07-17 19:49:26 +0800

    Update: user setting UI update

--- src/pages/team/components/user/UserSettings.vue
+++ src/pages/team/components/user/UserSettings.vue
@@ -1,73 +1,50 @@
 <template>
-  <div style="min-width: 40vw">
+  <div :style="$q.screen.gt.xs ? 'min-width: 40vw' : ''" :class="$q.screen.gt.xs ? '' : 'fit'">
     <q-card
-      bordered
-      class="z-max"
-      :class="
-        $q.dark.mode ? 'bg-grey-10 text-grey-1' : 'bg-grey-1 text-grey-10'
-      "
+      :bordered="$q.screen.gt.xs"
+      class="z-max no-wrap"
+      :class="`
+        ${$q.dark.mode ? 'text-grey-1' : 'bg-grey-1 text-grey-10'}
+        ${$q.screen.gt.xs ? 'row' : 'column fit'}
+      `"
+      :style="$q.screen.gt.xs ? 'min-height: 40vh' : ''"
     >
-      <div class="fit row no-wrap">
-        <q-list
-          class="border-right q-pa-sm column gap-xs"
-          style="min-width: 160px; min-height: 36rem"
+      <section
+        class="q-pa-xs"
+        :class="!$q.screen.gt.xs ? 'border-bottom row no-wrap' : 'border-right'"
+      >
+        <q-tabs
+          v-model="section"
+          inline-label
+          :vertical="$q.screen.gt.xs"
+          :active-class="$q.screen.gt.xs ? 'border' : ''"
         >
-          <q-item
-            clickable
-            v-ripple
-            @click="section = 'theme'"
-            class="radius-xs"
-            :class="section === 'theme' ? 'bg-primary text-white' : ''"
-          >
-            <q-item-section side>
-              <q-icon
-                name="mdi-theme-light-dark"
-                :color="section === 'theme' ? 'white' : ''"
-              />
-            </q-item-section>
-            <q-item-section>主题</q-item-section>
-          </q-item>
-          <q-item
-            clickable
-            v-ripple
-            @click="section = 'profile'"
-            class="radius-xs"
-            :class="section === 'profile' ? 'bg-primary text-white' : ''"
-          >
-            <q-item-section side>
-              <q-icon
-                name="info"
-                :color="section === 'profile' ? 'white' : ''"
-              />
-            </q-item-section>
-            <q-item-section>个人资料</q-item-section>
-          </q-item>
-          <q-item
-            clickable
-            v-ripple
-            @click="section = 'others'"
-            class="radius-xs"
-            :class="section === 'others' ? 'bg-primary text-white' : ''"
-          >
-            <q-item-section side>
-              <q-icon
-                name="mdi-dots-vertical"
-                :color="section === 'others' ? 'white' : ''"
-              />
-            </q-item-section>
-            <q-item-section>其它</q-item-section>
-          </q-item>
-        </q-list>
-        <div class="fit column no-wrap q-space q-pa-sm">
-          <div class="q-pa-sm row no-wrap items-center">
-            <q-space />
-            <q-btn flat icon="close" dense size="sm" round v-close-popup />
-          </div>
-          <UserTheme v-if="section === 'theme'" />
-          <UpdateProfile v-if="section === 'profile'" />
-          <OtherSetting v-if="section === 'others'" />
-        </div>
-      </div>
+          <q-tab
+            v-for="i in sections"
+            :key="i.val" :name="i.val" :label="i.label" :icon="i.icon"
+            :class="$q.screen.gt.xs ? 'radius-xs overflow-hidden' : ''"
+          />
+        </q-tabs>
+        <q-space />
+        <q-btn v-if="!$q.screen.gt.xs" flat icon="close" dense size="sm" round v-close-popup />
+      </section>
+      <q-scroll-area class="column no-wrap q-space">
+        <q-bar v-if="$q.screen.gt.xs" class="transparent">
+          <q-space />
+          <q-btn flat icon="close" dense size="sm" round v-close-popup />
+        </q-bar>
+        <q-tab-panels v-model="section" animated transition-prev="fade" transition-next="fade">
+          <q-tab-panel name="theme">
+            <UserTheme />
+          </q-tab-panel>
+          <q-tab-panel name="profile">
+            <UpdateProfile />
+          </q-tab-panel>
+          <q-tab-panel name="others">
+            <OtherSetting />
+          </q-tab-panel>
+        </q-tab-panels>
+      </q-scroll-area>
     </q-card>
   </div>
 </template>
@@ -79,6 +56,9 @@ import UpdateProfile from "./Settings/UpdateProfile.vue";
 import OtherSetting from "./Settings/OtherSetting.vue";
 
 const section = ref("theme");
+const sections = [
+  {val: 'theme', label: '主题', icon: 'mdi-theme-light-dark'},
+  {val: 'profile', label: '个人资料', icon: 'info'},
+  {val: 'others', label: '其它', icon: 'mdi-dots-vertical'},
+]
 </script>
-
-<style lang="scss" scoped></style>
