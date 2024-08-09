15ba205e2d0751f3bdbdeda150f004c5fbcb042f
jooler <jerr@foxmail.com>
2024-08-09 18:37:12 +0800

    Update: get ws clintID Logic

--- src/stores/mmws.js
+++ src/stores/mmws.js
@@ -1,4 +1,6 @@
 import { defineStore } from "pinia";
+import { uid } from 'quasar'
+
 export default defineStore("mmws", {
   state: () => ({
     event: {
@@ -8,7 +10,7 @@ export default defineStore("mmws", {
     },
     // 使用浏览器指纹作为客户端ID，用来在ws事件时判断是否是当前用户操作的ws内容
     // 如果是，当前用户的对应操作已经执行了，就不必执行ws的数据更新事件了
-    clientId: window.fingerprint, 
+    clientId: window.fingerprint + '_' + uid(), 
     ws: null,
     log: [],
     online: false,
