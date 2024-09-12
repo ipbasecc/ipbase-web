bf77899e728f497d1f71ae782884862380608122
jooler <jerr@foxmail.com>
2024-09-13 02:35:37 +0800

    输入框可以ctrl z

--- src/components/Utilits/InputDiv.vue
+++ src/components/Utilits/InputDiv.vue
@@ -2,6 +2,7 @@
   <div
     ref="editableDiv"
     :contenteditable="auth"
+    v-mutation="handler"
     :class="`${baseClass} ${auth ? activeClass : ''}`"
     @input="updateValue"
     @keydown.esc="ESC"
@@ -14,8 +15,9 @@
 </template>
 
 <script setup>
-import {defineModel, onMounted, useTemplateRef, nextTick} from 'vue';
+import {ref, onMounted, useTemplateRef} from 'vue';
 import {removeHtmlTags} from "src/hooks/utilits.js";
+import { onKeyStroke } from "@vueuse/core";
 
 const { baseClass, activeClass, auth, autofocus } = defineProps({
   baseClass: {
@@ -121,4 +123,82 @@ const handlePaste = async (event) => {
     modelValue.value = editableDiv.value.innerText;
   }
 };
+const maxStack = ref(100)
+const undoStack = ref([])
+const redoStack = ref([])
+const undoBlocked = ref(false)
+
+function checkStack (stack) {
+  if (stack.length > maxStack.value) {
+    stack.splice(maxStack.value)
+  }
+}
+
+function clearStack (stack) {
+  stack.splice(0)
+}
+
+function undo() {
+  if (undoStack.value.length > 0) {
+    const data = undoStack.value.shift();
+    if (data !== void 0) {
+      undoBlocked.value = true;
+      editableDiv.value.innerText = data;
+      modelValue.value = data;
+      moveCursorToEnd()
+    }
+  }
+};
+
+
+function redo () {
+  // shift the stack
+  const data = redoStack.value.shift()
+  if (data !== void 0) {
+    // unblock undo from receiving redo data
+    undoBlocked.value = false
+    editableDiv.value.innerText = data
+  }
+};
+function moveCursorToEnd() {
+  const _editableDiv = editableDiv.value; // 获取输入框的DOM元素
+  if (_editableDiv) {
+    const range = document.createRange(); // 创建一个新的Range对象
+    const selection = window.getSelection(); // 获取当前的选择对象
+
+    range.selectNodeContents(_editableDiv); // 选择输入框内的所有内容
+    range.collapse(false); // 折叠Range到末尾
+
+    selection.removeAllRanges(); // 移除当前的所有选择范围
+    selection.addRange(range); // 添加新的Range到选择中
+  }
+}
+
+function handler(mutationRecords) {
+  mutationRecords.forEach(record => {
+    if (record.type === 'characterData') {
+      undoStack.value.unshift(record.oldValue);
+      checkStack(undoStack.value);
+      clearStack(redoStack.value);
+    }
+    else if (record.type === 'childList') {
+      record.removedNodes.forEach(node => {
+        if (undoBlocked.value === false) {
+          undoStack.value.unshift(node.textContent);
+        } else {
+          redoStack.value.unshift(node.textContent);
+        }
+      });
+      checkStack(undoStack.value);
+      checkStack(redoStack.value);
+      undoBlocked.value = false;
+    }
+  });
+}
+
+// 修改onKeyStroke监听器，添加saveSelection调用
+onKeyStroke(["ctrlKey", "z"], (e) => {
+  e.preventDefault();
+  undo();
+});
 </script>
--- src/pages/team/todo/TodoInput.vue
+++ src/pages/team/todo/TodoInput.vue
@@ -3,6 +3,7 @@
     <div
       :contenteditable="editable"
       ref="classlessInput"
+      v-mutation="handler"
       style="writing-mode: lr;cursor: text"
       :class="`${props.baseClass} ${editable ? props.activeClass : ''}`"
       :autofocus="autofocus"
@@ -21,6 +22,7 @@
 import {ref, onMounted, toRefs, watchEffect} from 'vue';
 import { removeHtmlTags } from "src/hooks/utilits.js";
 import {uiStore} from 'src/hooks/global/useStore';
+import { onKeyStroke } from "@vueuse/core";
 
 const props = defineProps({
   content: {
@@ -123,8 +125,8 @@ const handlePaste = async (event) => {
     selection.removeAllRanges();
     selection.addRange(selectedRange);
 
-    // 更新modelValue
-    modelValue.value = editableDiv.value.innerText;
+    // 更新inputValue
+    inputValue.value = classlessInput.value.innerText;
   } catch (err) {
     console.error('Failed to read clipboard contents: ', err);
     // 兼容旧浏览器的粘贴操作
@@ -153,8 +155,87 @@ const handlePaste = async (event) => {
     selection.removeAllRanges();
     selection.addRange(selectedRange);
 
-    // 更新modelValue
-    modelValue.value = editableDiv.value.innerText;
+    // 更新inputValue
+    inputValue.value = classlessInput.value.innerText;
   }
 };
+
+const maxStack = ref(100)
+const undoStack = ref([])
+const redoStack = ref([])
+const undoBlocked = ref(false)
+
+function checkStack (stack) {
+  if (stack.length > maxStack.value) {
+    stack.splice(maxStack.value)
+  }
+}
+
+function clearStack (stack) {
+  stack.splice(0)
+}
+
+function undo() {
+  if (undoStack.value.length > 0) {
+    const data = undoStack.value.shift();
+    if (data !== void 0) {
+      undoBlocked.value = true;
+      classlessInput.value.innerText = data;
+      inputValue.value = data;
+      moveCursorToEnd()
+    }
+  }
+};
+
+
+function redo () {
+  // shift the stack
+  const data = redoStack.value.shift()
+  if (data !== void 0) {
+    // unblock undo from receiving redo data
+    undoBlocked.value = false
+    classlessInput.value.innerText = data
+  }
+};
+function moveCursorToEnd() {
+  const _classlessInput = classlessInput.value; // 获取输入框的DOM元素
+  if (_classlessInput) {
+    const range = document.createRange(); // 创建一个新的Range对象
+    const selection = window.getSelection(); // 获取当前的选择对象
+
+    range.selectNodeContents(_classlessInput); // 选择输入框内的所有内容
+    range.collapse(false); // 折叠Range到末尾
+
+    selection.removeAllRanges(); // 移除当前的所有选择范围
+    selection.addRange(range); // 添加新的Range到选择中
+  }
+}
+
+function handler(mutationRecords) {
+  mutationRecords.forEach(record => {
+    if (record.type === 'characterData') {
+      undoStack.value.unshift(record.oldValue);
+      checkStack(undoStack.value);
+      clearStack(redoStack.value);
+    }
+    else if (record.type === 'childList') {
+      record.removedNodes.forEach(node => {
+        if (undoBlocked.value === false) {
+          undoStack.value.unshift(node.textContent);
+        } else {
+          redoStack.value.unshift(node.textContent);
+        }
+      });
+      checkStack(undoStack.value);
+      checkStack(redoStack.value);
+      undoBlocked.value = false;
+    }
+  });
+}
+
+// 修改onKeyStroke监听器，添加saveSelection调用
+onKeyStroke(["ctrlKey", "z"], (e) => {
+  e.preventDefault();
+  undo();
+});
 </script>
