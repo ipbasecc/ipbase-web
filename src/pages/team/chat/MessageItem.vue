0908ad88d4825742acc857baf94ce8bae7c2608b
jooler <jerr@foxmail.com>
2024-10-24 15:20:24 +0800

    系统消息样式优化

--- src/pages/team/chat/MessageItem.vue
+++ src/pages/team/chat/MessageItem.vue
@@ -1,187 +1,191 @@
 <template>
-  <div
-    v-if="msg?.message"
-    v-bind="$attrs"
-    class="row no-wrap gap-md q-py-xs q-px-md hovered-item"
-    :class="`${is_sameUser ? '' : 'q-mt-sm'} ${
-      curThreadId === msg.id ? 'active' : ''
-    }
-    ${!msg?.root_id ? 'mm_message' : msg.props?.strapi ?'unselected' : ''}`"
+  <div v-if="msg?.message" v-bind="$attrs"
+    class="row no-wrap gap-md q-py-xs q-px-md"
+    :class="`
+      ${is_sameUser ? '' : 'q-mt-sm'}
+      ${msg.type.includes('system_') ? '' : 'hovered-item'}
+      ${curThreadId === msg.id ? 'active' : ''}
+      ${!msg?.root_id && !msg.type.includes('system_') ? 'mm_message' : msg.props?.strapi ? 'unselected' : ''}
+    `"
   >
-    <template v-if="show_avatar">
-      <template v-if="isExternal">
-        <q-avatar
-          v-if="!isMe"
-          :size="`${avatar_size}px`"
-          color="teal"
-          icon="mdi-account"
-        />
-        <q-avatar v-else :size="`${avatar_size}px`" color="primary">
-          {{$t('you')}}
-        </q-avatar>
-      </template>
-      <UserAvatar v-else
-        :user_id="msg.user_id"
-        :size="avatar_size"
-        :status="member_status"
-        :strapi_member
-        :class="!MsgOnly && $q.screen.gt.xs ? 'q-ml-xl' : ''"
-      />
+    <template v-if="msg.type.includes('system_')">
+      <div class="full-width flex flex-center op-5">{{ msg.message }}</div>
     </template>
     <template v-else>
-      <div class="hover-show transition row items-center justify-end text-grey-6"
-        :style="`min-width: ${avatar_size + (!MsgOnly ? 48 : 0)}px;`"
+      <template v-if="show_avatar">
+        <template v-if="isExternal">
+          <q-avatar v-if="!isMe"
+            :size="`${avatar_size}px`"
+            color="teal"
+            icon="mdi-account"
+          />
+          <q-avatar v-else :size="`${avatar_size}px`" color="primary">
+            {{$t('you')}}
+          </q-avatar>
+        </template>
+        <UserAvatar v-else
+          :user_id="msg.user_id"
+          :size="avatar_size"
+          :status="member_status"
+          :strapi_member
+          :class="!MsgOnly && $q.screen.gt.xs ? 'q-ml-xl' : ''"
+        />
+      </template>
+      <template v-else>
+        <div class="hover-show transition row items-center justify-end text-grey-6"
+          :style="`min-width: ${avatar_size + (!MsgOnly ? 48 : 0)}px;`"
+        >
+          <TimeAgo v-if="!MsgOnly" :time="msg.create_at" />
+        </div>
+        <div v-if="msg.props?.strapi" class="hover-hide transition absolute row items-center justify-end text-grey-6 hover-hide transition"
+          :style="`min-width: ${avatar_size + (!MsgOnly ? 48 : 0)}px;`">
+          <q-icon name="mdi-information-outline" />
+        </div>
+      </template>
+      <div class="column no-wrap gap-xs justify-between q-space relative-position"
+        @mouseover="wiget_show = true"
+        @mouseleave="wiget_show = false"
       >
-        <TimeAgo v-if="!MsgOnly" :time="msg.create_at" />
+        <span v-if="show_avatar" class="op-5">
+          <template v-if="!isExternal"> {{ member?.username }}</template>
+          <template v-if="!MsgOnly"> · 
+            <TimeAgo :time="msg.create_at" />
+          </template>
+        </span>
+        <div
+          v-html="html"
+          :class="!msg?.root_id ? 'cursor-pointer' : ''"
+          @click="enterThread(msg)"
+        ></div>
+        <template v-if="msg.metadata?.files?.length > 0">
+          <showFile :files="msg.metadata?.files" />
+        </template>
+        <!-- bottom info-->
+        <div class="row no-wrap gap-xs items-center op-3">
+          <div
+            v-if="msg.is_pinned && container !== 'pinned'"
+            class="row no-wrap gap-xs items-center cursor-pointer"
+            @click="togglePowerpannel('pinneds')"
+          >
+            <q-icon name="push_pin" color="primary" />
+            <span class="hover-height transition">{{ $t('pinned') }}</span>
+          </div>
+          <div
+            v-if="flagged(msg) && container !== 'flagged'"
+            class="row no-wrap gap-xs items-center cursor-pointer"
+            @click="togglePowerpannel('flaggeds')"
+          >
+            <q-icon name="bookmark" color="primary" />
+            <span class="hover-height transition">{{ $t('favorited') }}</span>
+          </div>
+          <template v-if="msg?.reply_count > 0 && !msg?.root_id">
+            <q-chip icon="mdi-reply" dense :label="msg.reply_count" clickable @click="enterThread(msg)" />
+          </template>
+        </div>
+        <!-- float action btns-->
+        <div
+          v-if="(wiget_show || menu_expend) && !MsgOnly"
+          class="absolute transition blur-sm mm_message_wiget radius-sm border overflow-hidden"
+          style="right: 10%; bottom: 0"
+        >
+          <div class="row no-wrap gap-xs text-white q-pa-xs mm_message_wiget">
+            <q-btn
+              dense
+              flat
+              icon="bookmark"
+              size="0.7rem"
+              :color="flagged(msg) ? 'primary' : 'gery-3'"
+              @click="toggle_flagged(msg)"
+            />
+            <q-btn
+              dense
+              flat
+              icon="push_pin"
+              size="0.7rem"
+              :color="msg.is_pinned ? 'deep-orange' : 'gery-3'"
+              @click="toggle_pin(msg)"
+            />
+            <q-btn
+              dense
+              flat
+              icon="reply"
+              size="0.7rem"
+              :color="flagged(msg) ? 'primary' : 'gery-3'"
+              @click="enterThread(msg)"
+            />
+            <q-btn dense flat icon="more_vert" size="0.7rem">
+              <q-menu v-model="menu_expend">
+                <q-list
+                  bordered
+                  dense
+                  style="min-width: 100px"
+                  class="radius-sm q-pa-xs"
+                >
+                  <q-item
+                    clickable
+                    v-close-popup
+                    class="radius-xs"
+                    @click="enterThread(msg)"
+                  >
+                    <q-item-section side><q-icon name="reply" /></q-item-section>
+                    <q-item-section>{{$t('reply')}}</q-item-section>
+                  </q-item>
+                  <q-item
+                    clickable
+                    v-close-popup
+                    class="radius-xs"
+                    @click="followThreadFn(msg)"
+                  >
+                    <q-item-section side
+                      ><q-icon name="mdi-heart-pulse"
+                    /></q-item-section>
+                    <q-item-section>{{$t('follow_thread')}}</q-item-section>
+                  </q-item>
+                  <q-item
+                    clickable
+                    v-close-popup
+                    class="radius-xs"
+                    @click="toggle_flagged(msg)"
+                  >
+                    <q-item-section side
+                      ><q-icon name="mdi-book-plus"
+                    /></q-item-section>
+                    <q-item-section>{{
+                      flagged(msg) ? $t('unfavorite_message') : $t('favorite_message')
+                    }}</q-item-section>
+                  </q-item>
+                  <q-item
+                    clickable
+                    v-close-popup
+                    class="radius-xs"
+                    @click="toggle_pin(msg)"
+                  >
+                    <q-item-section side
+                      ><q-icon name="mdi-pin"
+                    /></q-item-section>
+                    <q-item-section>{{
+                      msg.is_pinned ? $t('unpin_message') : $t('pin_message')
+                    }}</q-item-section>
+                  </q-item>
+                </q-list>
+              </q-menu>
+            </q-btn>
+          </div>
+        </div>
       </div>
-      <div v-if="msg.props?.strapi" class="hover-hide transition absolute row items-center justify-end text-grey-6 hover-hide transition"
-        :style="`min-width: ${avatar_size + (!MsgOnly ? 48 : 0)}px;`">
-        <q-icon name="mdi-information-outline" />
+      <div v-if="MsgOnly && container === 'flagged'">
+        <q-btn
+          dense
+          size="sm"
+          flat
+          round
+          icon="mdi-bookmark-remove"
+          color="primary"
+          class="hover-show transition"
+          @click="toggle_flagged(msg)"
+        />
       </div>
     </template>
-    <div class="column no-wrap gap-xs justify-between q-space relative-position"
-      @mouseover="wiget_show = true"
-      @mouseleave="wiget_show = false"
-    >
-      <span v-if="show_avatar" class="op-5">
-        <template v-if="!isExternal"> {{ member?.username }}</template>
-        <template v-if="!MsgOnly"> · 
-          <TimeAgo :time="msg.create_at" />
-        </template>
-      </span>
-      <div
-        v-html="html"
-        :class="!msg?.root_id ? 'cursor-pointer' : ''"
-        @click="enterThread(msg)"
-      ></div>
-      <template v-if="msg.metadata?.files?.length > 0">
-        <showFile :files="msg.metadata?.files" />
-      </template>
-      <!-- bottom info-->
-      <div class="row no-wrap gap-xs items-center op-3">
-        <div
-          v-if="msg.is_pinned && container !== 'pinned'"
-          class="row no-wrap gap-xs items-center cursor-pointer"
-          @click="togglePowerpannel('pinneds')"
-        >
-          <q-icon name="push_pin" color="primary" />
-          <span class="hover-height transition">{{ $t('pinned') }}</span>
-        </div>
-        <div
-          v-if="flagged(msg) && container !== 'flagged'"
-          class="row no-wrap gap-xs items-center cursor-pointer"
-          @click="togglePowerpannel('flaggeds')"
-        >
-          <q-icon name="bookmark" color="primary" />
-          <span class="hover-height transition">{{ $t('favorited') }}</span>
-        </div>
-        <template v-if="msg?.reply_count > 0 && !msg?.root_id">
-          <q-chip icon="mdi-reply" dense :label="msg.reply_count" clickable @click="enterThread(msg)" />
-        </template>
-      </div>
-      <!-- float action btns-->
-      <div
-        v-if="(wiget_show || menu_expend) && !MsgOnly"
-        class="absolute transition blur-sm mm_message_wiget radius-sm border overflow-hidden"
-        style="right: 10%; bottom: 0"
-      >
-        <div class="row no-wrap gap-xs text-white q-pa-xs mm_message_wiget">
-          <q-btn
-            dense
-            flat
-            icon="bookmark"
-            size="0.7rem"
-            :color="flagged(msg) ? 'primary' : 'gery-3'"
-            @click="toggle_flagged(msg)"
-          />
-          <q-btn
-            dense
-            flat
-            icon="push_pin"
-            size="0.7rem"
-            :color="msg.is_pinned ? 'deep-orange' : 'gery-3'"
-            @click="toggle_pin(msg)"
-          />
-          <q-btn
-            dense
-            flat
-            icon="reply"
-            size="0.7rem"
-            :color="flagged(msg) ? 'primary' : 'gery-3'"
-            @click="enterThread(msg)"
-          />
-          <q-btn dense flat icon="more_vert" size="0.7rem">
-            <q-menu v-model="menu_expend">
-              <q-list
-                bordered
-                dense
-                style="min-width: 100px"
-                class="radius-sm q-pa-xs"
-              >
-                <q-item
-                  clickable
-                  v-close-popup
-                  class="radius-xs"
-                  @click="enterThread(msg)"
-                >
-                  <q-item-section side><q-icon name="reply" /></q-item-section>
-                  <q-item-section>{{$t('reply')}}</q-item-section>
-                </q-item>
-                <q-item
-                  clickable
-                  v-close-popup
-                  class="radius-xs"
-                  @click="followThreadFn(msg)"
-                >
-                  <q-item-section side
-                    ><q-icon name="mdi-heart-pulse"
-                  /></q-item-section>
-                  <q-item-section>{{$t('follow_thread')}}</q-item-section>
-                </q-item>
-                <q-item
-                  clickable
-                  v-close-popup
-                  class="radius-xs"
-                  @click="toggle_flagged(msg)"
-                >
-                  <q-item-section side
-                    ><q-icon name="mdi-book-plus"
-                  /></q-item-section>
-                  <q-item-section>{{
-                    flagged(msg) ? $t('unfavorite_message') : $t('favorite_message')
-                  }}</q-item-section>
-                </q-item>
-                <q-item
-                  clickable
-                  v-close-popup
-                  class="radius-xs"
-                  @click="toggle_pin(msg)"
-                >
-                  <q-item-section side
-                    ><q-icon name="mdi-pin"
-                  /></q-item-section>
-                  <q-item-section>{{
-                    msg.is_pinned ? $t('unpin_message') : $t('pin_message')
-                  }}</q-item-section>
-                </q-item>
-              </q-list>
-            </q-menu>
-          </q-btn>
-        </div>
-      </div>
-    </div>
-    <div v-if="MsgOnly && container === 'flagged'">
-      <q-btn
-        dense
-        size="sm"
-        flat
-        round
-        icon="mdi-bookmark-remove"
-        color="primary"
-        class="hover-show transition"
-        @click="toggle_flagged(msg)"
-      />
-    </div>
   </div>
 </template>
 
