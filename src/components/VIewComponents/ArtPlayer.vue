61a916cd9ccfc3a83933c1ae5fdaae7f5fdaa678
jooler <jerr@foxmail.com>
2024-11-29 16:30:03 +0800

    防内存泄漏

--- src/components/VIewComponents/ArtPlayer.vue
+++ src/components/VIewComponents/ArtPlayer.vue
@@ -1,48 +1,44 @@
 <template>
-  <div ref="artRef"></div>
+  <div v-show="instance" ref="ArtplayerRef"></div>
 </template>
 
 <script setup>
-import { ref, toRefs, onMounted, onBeforeUnmount, nextTick } from 'vue'
-import Artplayer from "artplayer";
+  import { ref, toRefs, onMounted, onBeforeUnmount, nextTick, useTemplateRef } from 'vue'
+  import Artplayer from "artplayer";
 
-const props = defineProps({
-  option: {
+  const props = defineProps({
+    option: {
       type: Object,
       required: true,
-  }
-});
-const emit = defineEmits(['get-instance', 'fullscreenWeb']);
-const { option } = toRefs(props);
-const artRef = ref(null); // 创建一个响应式引用作为DOM元素的引用
-const instance = ref(null); // 创建一个响应式引用来存储Artplayer实例
+    }
+  });
+  const emit = defineEmits(['get-instance', 'fullscreenWeb']);
+  const { option } = toRefs(props);
+  const ArtplayerRef = useTemplateRef('ArtplayerRef'); // 创建一个响应式引用作为DOM元素的引用
+  const instance = ref(null); // 创建一个响应式引用来存储Artplayer实例
 
-onMounted(() => {
-  instance.value = new Artplayer({
+  onMounted(() => {
+    instance.value = new Artplayer({
       ...option.value,
-      container: artRef.value,
-  });
+      container: ArtplayerRef.value,
+    });
 
-  nextTick(() => {
+    nextTick(() => {
       emit('get-instance', instance.value);
-  });
+    });
 
-  instance.value.on('fullscreenWeb', (state) => {
+    instance.value.on('fullscreenWeb', (state) => {
       emit('fullscreenWeb', state);
+    });
+
+    instance.value.on('fullscreen', (state) => {
+      emit('fullscreen', state);
+    });
   });
 
-  instance.value.on('fullscreen', (state) => {
-    emit('fullscreen', state);
-  });
-});
-
-onBeforeUnmount(() => {
-  if (instance.value && instance.value.destroy) {
+  onBeforeUnmount(() => {
+    if (instance.value && instance.value.destroy) {
       instance.value.destroy(false);
-  }
-});
-
+    }
+  });
 </script>
-
-<style scoped>
-</style>
