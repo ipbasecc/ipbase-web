514a6925002a9d8606397622050ae5dc101361de
jooler <jerr@foxmail.com>
2024-12-19 18:44:13 +0800

    deal UI调整

--- src/pages/deliver/components/PartyaCard.vue
+++ src/pages/deliver/components/PartyaCard.vue
@@ -9,14 +9,16 @@
         spinner-size="82px"
         :class="deal.cover?.url ? '' : 'op-5'"
     >
-    <div v-if="deal.description" class="column no-wrap gap-sm absolute-bottom">
-        <div class="text-subtitle2 cursor-pointer q-pa-md" @click="toDeal">{{ deal.description }}</div>
-        <div v-if="deal.tags?.length > 0" class="row items-center">
-            <q-chip v-for="(tag, index) in deal.tags" :key="index"
-            square color="primary" :label="tag"
-            :class="index === 0 ? 'q-ml-none' : index === deal.tags.length - 1 ? 'q-mr-none' : ''"
-            />
-        </div>
+    <div v-if="deal.description" class="absolute-bottom row no-wrap">
+        <section class="q-space column no-wrap gap-sm q-pa-md">
+            <div class="text-subtitle2 cursor-pointer" @click="toDeal">{{ deal.description }}</div>
+            <div v-if="deal.tags?.length > 0" class="row items-center">
+                <q-chip v-for="(tag, index) in deal.tags" :key="index"
+                square color="primary" :label="tag"
+                :class="index === 0 ? 'q-ml-none' : index === deal.tags.length - 1 ? 'q-mr-none' : ''"
+                />
+            </div>
+        </section>
     </div>
     </q-img>
     <q-card-section class="cursor-pointer" >
@@ -25,7 +27,7 @@
                 <UserAvatar
                     :image="deal.party_a?.profile?.avatar?.url"
                     :user_id="deal.party_a?.mm_profile?.id"
-                    :size="32"
+                    :size="26"
                     :disable_cardRef="true"
                     :mm_member="deal.party_a?.mm_profile"
                 />
@@ -35,8 +37,8 @@
             </q-item-section>
         </q-item>
         <div class="text-h6 cursor-pointer" @click="toDeal">{{ deal.name }}</div>
-        <div class="row items-center">
-            <span>￥：</span>
+        <div class="row items-baseline">
+            <span class="bg-orange radius-xs border q-px-xs q-mr-xs" style="transform: translateY(-4px);">￥</span>
             <span class="text-h4">{{ deal.amount / 100 }}</span>
         </div>
     </q-card-section>
